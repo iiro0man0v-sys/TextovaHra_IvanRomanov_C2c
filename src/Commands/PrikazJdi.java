b269d9179ca1c0acc9f6e6572f9f37cd61734e5d
roman <iiro0man0v@gmail.com>
2026-01-25 22:08:41 +0100

    Prikaz jdi

--- src/Commands/PrikazJdi.java
+++ src/Commands/PrikazJdi.java
@@ -1,6 +1,10 @@
 package Commands;
 
 import Else.Nacitani;
+import Else.Prostor;
+
+import java.util.ArrayList;
+
 
 public class PrikazJdi implements IPrikaz {
     private Nacitani n;
@@ -8,10 +12,35 @@ public class PrikazJdi implements IPrikaz {
     public PrikazJdi(Nacitani n) {
         this.n = n;
     }
-
+    /**
+     * Provede přesun do sousední místnosti.
+     * @param parametry název cílové místnosti.
+     * @return zpráva pro hráče o výsledku pohybu.
+     */
     @Override
-    public String proved(String[] parametry) {
-        return "";
+    public String proved(ArrayList<String> parametry) {
+
+        if (parametry.isEmpty()) {
+            return "Kam mám jít? Musíš zadat název místnosti (např. jdi knihovna).";
+        }else {
+            for (int i = 0; i < parametry.size(); i++) {
+                Prostor aktualni = n.getAktualniProstor();
+                Prostor sousedni = aktualni.vratSousedniProstor(parametry.get(i));
+
+                if (sousedni == null) {
+                    return "Tam se odsud jít nedá! Zkus jiný směr.";
+                } else {
+                    n.setAktualniProstor(sousedni);
+                    return sousedni.dlouhyPopis();
+                }
+            }
+            return null;
+        }
+
+
+
+
+
     }
 
     @Override
