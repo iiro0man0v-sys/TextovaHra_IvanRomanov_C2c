aae4698bcdb37be103e49ffdd8d88aba9136fb73
roman <iiro0man0v@gmail.com>
2026-01-25 21:25:16 +0100

    Nacteni dat ze souboru

--- src/Else/Nacitani.java
+++ src/Else/Nacitani.java
@@ -1,16 +1,68 @@
 package Else;
 
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Nacitani {
     private Prostor aktualniProstor;
     private Inventar batoh;
+    private Map<String, Prostor> vsechnyProstory;
 
     public void nacitani() {
         batoh = new Inventar();
+        this.vsechnyProstory = new HashMap<>();
+
 
     }
 
-    public void nactiZeSouboru(String cesta) {
+    /**
+     * Metoda načte data z JSON souboru a sestaví herní svět.
+     * @param cesta cesta k souboru (např. "/gamedata.json")
+     */
+    public void nactiZeSouboru(String cesta) throws IOException {
+        Gson gson = new Gson();
+        try (InputStream is = Nacitani.class.getResourceAsStream(cesta)) {
+            if (is == null) {
+                throw new RuntimeException("Soubor nebyl nalezen: " + cesta);
+            }
 
+            // Načtení dat do pomocné struktury
+            GameData data = gson.fromJson(
+                    new InputStreamReader(is, StandardCharsets.UTF_8),
+                    GameData.class
+            );
+
+
+            for (ProstorData pData : data.locations) {
+                Prostor novy = new Prostor(pData.nazev, pData.popis);
+
+                if (pData.predmety != null) {
+                    for (Vec v : pData.predmety) novy.vlozVec(v);
+                }
+
+                if (pData.postavy != null) {
+                    for (Postava pos : pData.postavy) novy.vlozPostavu(pos);
+                }
+
+                vsechnyProstory.put(pData.id, novy);
+            }
+
+            for (ProstorData pData : data.locations) {
+                Prostor zdroj = vsechnyProstory.get(pData.id);
+                for (String cilId : pData.vychody) {
+                    Prostor cil = vsechnyProstory.get(cilId);
+                    if (cil != null) {
+                        zdroj.setVychod(cil);
+                    }
+                }
+            }
+        }
     }
 
     public Prostor getAktualniProstor() {
